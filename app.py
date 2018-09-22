caaf74c634ded7c7e8d5a045fe27d6de4ae48659
eliasrosendahl <jensen.eliasrosendahl@gmail.com>
2018-09-22 12:32:56 +0200

    client now loads chat history when connecting

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
 
 app = Flask(__name__)
-
 socketio = SocketIO(app)
+
+chatLog = ""
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@socketio.on("connectEvent")
+def handle_new_connection(json):
+    global chatLog
+    emit("connectResponce", chatLog)
+
 @socketio.on("message")
 def handle_event(json):
+    global chatLog
+    chatLog += json['msg'] + "\n"
     emit("responce", json['msg'], broadcast=True)
 
 
